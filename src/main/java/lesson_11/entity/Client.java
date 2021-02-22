67d34fc5fca8c21f606aa2f78b2b9725699f2ba2
BohdanovychD <dima.bogdanovych2001@gmail.com>
2021-02-23 00:15:19 +0200

    Using try/catch and Exeptions

---
+++ src/main/java/lesson_11/entity/Client.java
@@ -0,0 +1,42 @@
+package lesson_11.entity;
+
+public class Client {
+
+    private String clientIdentify;
+    private String secondName;
+    private String clientAccIdentify;
+    private double sum;
+
+    public String getClientIdentify() {
+        return clientIdentify;
+    }
+
+    public void setClientIdentify(String clientIdentify) {
+        this.clientIdentify = clientIdentify;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getClientAccIdentify() {
+        return clientAccIdentify;
+    }
+
+    public void setClientAccIdentify(String clientAcc) {
+        this.clientAccIdentify = clientAcc;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+}
